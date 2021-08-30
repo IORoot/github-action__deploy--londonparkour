01da9adf685d9c51e9d55ff2307a5c17ff2e7995
Andy Pearson <andy@londonparkour.com>
2021-08-30 12:03:49 +0100

    deploy to master branch

---
+++ wp-content/plugins/real-media-library/vendor/devowl-wp/utils/vendor/composer/installed.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace MatthiasWeb\RealMediaLibrary\Vendor;
+
+return [
+    'root' => [
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'aliases' => [],
+        'reference' => 'f033d5cf7745968635115798454ee1f3466ccf5d',
+        'name' => 'devowl-wp/utils'
+    ],
+    'versions' => [
+        'devowl-wp/utils' => [
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'aliases' => [],
+            'reference' => 'f033d5cf7745968635115798454ee1f3466ccf5d'
+        ]
+    ]
+];
